fd77d5ded5582df7c23b68998bd02259771810d7
riche2000 <ricardosanch2000@gmail.com>
2022-12-30 13:09:41 -0600

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASPNET_MVC_SQLServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
